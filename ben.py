7632d441960f4c8780581d511652b0a203af8e32
Ryan Forsythe <ryan@pdxcb.net>
2011-06-27 10:41:55 -0700

    The results file wasn't getting cleared, skewing the results. Results fixed.

--- ben.py
+++ ben.py
@@ -5,14 +5,14 @@ import subprocess
 
 # First, build up the results file. it'll have the form:
 #      110 vidioc-g-dv-preset.xml
-linecount_cmd = "for i in *; do find $i -execdir wc -c '{}' \; " + \
+linecount_cmd = "rm results; for i in *; do find $i -execdir wc -c '{}' \; " + \
     ">> results; done;"
-os.system(linecount_cmd)
+#os.system(linecount_cmd)
 
 # Then figure out how many are in each bucket (linecount's initial digit)
 bucket_cmd = 'for i in `jot 9`; do egrep "^[ ]*$i" results| ' + \
     'wc -l; done  > counts'
-os.system(bucket_cmd)
+#os.system(bucket_cmd)
 
 # And how many total files there are...
 total_cmd = 'wc -l results'
@@ -21,7 +21,12 @@ p = subprocess.Popen(total_cmd, stdout=subprocess.PIPE, shell=True)
 
 total = int(total.split()[0])
 
-for i in file('counts', 'r').readlines():
-    print 100 * (float(i) / total)
+for i,count in enumerate(file('counts', 'r').readlines()):
+    print '"%d": %0.5f,' % (i + 1, 100 * (float(count) / total))
+
+print "Record count:", total
+
+os.system('echo "biggest:" && sort --general-numeric-sort -b results '
+          '| tail -n 1')
 
         
